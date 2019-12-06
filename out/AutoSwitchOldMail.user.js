a1bf0080b96cfc3650afc6c3324ec18d04eaf6cc
ra81 <ra8181@mail.ru>
2019-12-06 19:15:51 +0700

    Кривой инклуд почему то. Забыл noConflict дабы разные версии jquery не косякали.

--- out/AutoSwitchOldMail.user.js
+++ out/AutoSwitchOldMail.user.js
@@ -3,10 +3,11 @@
 // @namespace      virtonomica
 // @author         mr_Sumkin
 // @description    Замена линков ведущих на новую почту линками ко старому интерфейсу почты. Если задолбала новая почта то это ваш скрипт.
-// @include        http*://virtonomic*.*/
+// @include        http*://virtonomic*.*/*
 // @require        https://code.jquery.com/jquery-1.11.1.min.js
-// @version        1.0
+// @version        1.01
 // ==/UserScript==
+$ = jQuery = jQuery.noConflict(true);
 $(document).ready(() => {
     let $a = $(document.body).find("a[href$='/main/user/privat/persondata/message']");
     $a.each((i, el) => {
